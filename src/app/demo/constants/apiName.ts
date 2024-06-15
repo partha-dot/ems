183553d7f1a5d4d1655a82feb2918f6cad98b587
Partha <parthamaity32@gmail.com>
2024-06-15 10:32:51 +0530

    old code

--- src/app/demo/constants/apiName.ts
+++ src/app/demo/constants/apiName.ts
@@ -5,5 +5,6 @@ export const api_name = {
         CURRENT_USED:'client/devices/graphical_view/current',
         POWER_USED:'client/devices/graphical_view/power',
         TOTAL_POWER:'client/devices/graphical_view/total_power_analisis',
-        KVAR_KVA_KW:'client/devices/graphical_view/total_power_analisis'
+        KVAR_KVA_KW:'client/devices/graphical_view/total_power_analisis',
+        base_url:"http://13.49.80.167:8000/api"
 }
